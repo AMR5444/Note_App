c12d4b8d2d7bda11e68ec08c8091d4fc74fb5928
Amr Hamdan <your_email@example.com>
2025-09-17 16:27:07 +0300

    add note state

--- lib/cubits/cubit/add_notes_state.dart
+++ lib/cubits/cubit/add_notes_state.dart
@@ -4,3 +4,13 @@ part of 'add_notes_cubit.dart';
 sealed class NotesState {}
 
 final class NotesInitial extends NotesState {}
+
+final class NotesLoding extends NotesState {}
+
+final class NotesSuccsess extends NotesState {}
+
+final class NotesFaliure extends NotesState {
+  final String errorMessage;
+
+  NotesFaliure(this.errorMessage);
+}
