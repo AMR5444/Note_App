b5e6d80ea80989450465154f7c4721ce41c4bcfb
Amr Hamdan <your_email@example.com>
2025-09-17 17:16:30 +0300

    provid notes cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:test/cubits/cubit/add_notes_cubit.dart';
 import 'package:test/models/note_model.dart';
 import 'package:test/views/NotsPage_View.dart';
 import 'package:test/views/Widget/Constants.dart';
@@ -18,10 +20,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
-      home: NotspageView(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => NotesCubit()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
+        home: NotspageView(),
+      ),
     );
   }
 }
